eed66221ad9305d46df7d94f9bd71a42400bfaed
nicky ma <mazq@minstone.com.cn>
2020-11-20 18:17:37 +0800

    spring aop example

--- springAopExample/src/main/java/com/example/spring/aop/TestApplication.java
+++ springAopExample/src/main/java/com/example/spring/aop/TestApplication.java
@@ -1,7 +1,16 @@
 package com.example.spring.aop;
 
 
+import com.example.spring.aop.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestApplication {
+
+    public static void testAopProxy() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("classpath:spring_interfaces_config");
+        UserService userService = (UserService) ioc.getBean("userServiceProxy");
+    }
 
     public static void main(String[] args) {
 
--- springAopExample/src/main/java/com/example/spring/aop/service/UserService.java
+++ springAopExample/src/main/java/com/example/spring/aop/service/UserService.java
@@ -8,8 +8,8 @@ package com.example.spring.aop.service;
  * <pre>
  * @author mazq
  * 修改记录
- *    修改后版本:     修改人：  修改日期: 2020/11/20 17:57  修改内容:
+ *    修改后版本:     修改人：  修改日期: 2020/11/20 18:02  修改内容:
  * </pre>
  */
-public class UserService {
+public interface UserService {
 }
